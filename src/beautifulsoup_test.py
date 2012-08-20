e22e8172609673cd0d6c4245dc962b79c9998a5b
xbox <youxizhaopin@gmail.com>
2012-08-20 23:27:48 +0800

    beautiful soup test#1

--- src/beautifulsoup_test.py
+++ src/beautifulsoup_test.py
@@ -9,3 +9,25 @@ from BeautifulSoup import BeautifulSoup
 #       '<p id="secondpara" align="blah">This is paragraph<b>Two</b>',"</html>"]
 #soup = BeautifulSoup(''.join(doc))
 #print soup.prettify()
+#html = "<html><p>Para 1<p>Para 2<blockquote>Quote 1<blockquote>Quote2"
+#soup = BeautifulSoup(html)
+#print soup.prettify()
+#html = """
+#<html>
+#<form>
+# <table>
+# <td><input name="input1">Row 1 cell 1
+# <tr><td>Row 2 cell 1
+# </form> 
+# <td>Row 2 cell 2<br>This</br> sure is a long cell
+#</body> 
+#</html>"""
+#soup = BeautifulSoup(html)
+#print soup.prettify()
+#xml = "<doc><tag1>Contents1<tag2>Contents2<tag1>Contents3"
+#soup = BeautifulSoup(xml)
+#print soup.prettify()
+#soup = BeautifulSoup("Hello")
+#print soup.contents[0]
+#print soup.originalEncoding
+
